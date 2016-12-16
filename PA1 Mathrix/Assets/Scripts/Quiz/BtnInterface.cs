b211c273bc8307807526d698383da21c76ccf3d7
Artur MPA <deathmachine.travian@hotmail.com>
2016-12-16 14:04:15 +0000

    Text Quiz Write

--- PA1 Mathrix/Assets/Scripts/Quiz/BtnInterface.cs
+++ PA1 Mathrix/Assets/Scripts/Quiz/BtnInterface.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class BtnInterface : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    [SerializeField]
+    public Text factText;
+    int runOnce = 0;
+
+    GM_Object findObjQuestion;
+
+    void Start () {
+
+
+
+        //if (this.transform)
+        //factText.text = findObjQuestion.;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//if(runOnce == 0)
+ //       {
+ //           runOnce = 1;
+
+ //           findObjQuestion = GameObject.FindGameObjectWithTag("ListaQuizQuestions").GetComponent<GM_Object>();
+
+ //           Question questao = findObjQuestion.currentQuestion;
+ //           //Question findObjQuestion = GameObject.Find("ListaQuizQuestions").GetComponent(GM_Object).currentQuestion;
+ //           Debug.Log("Tag this transform " + transform.parent.parent.tag);
+ //           if (transform.parent.parent.tag == "OptionA")
+ //           {
+ //               factText.text = questao.solutionA;
+ //           }
+ //           if (transform.parent.parent.tag == "OptionB")
+ //           {
+ //               factText.text = questao.solutionB;
+
+ //           }
+ //           if (transform.parent.parent.tag == "OptionC")
+ //           {
+ //               factText.text = questao.solutionC;
+
+ //           }
+ //           if (transform.parent.parent.tag == "OptionD")
+ //           {
+ //               factText.text = questao.solutionD;
+
+ //           }
+ //       }
 	}
 }
--- PA1 Mathrix/Assets/getPossibleSolution.cs
+++ PA1 Mathrix/Assets/getPossibleSolution.cs
@@ -5,8 +5,11 @@ public class getPossibleSolution : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        //GameObject findObj = GameObject.Find("ListaQuizQuestions").GetComponent<G>();
+        //Question  GameObject.Find("ListaQuizQuestions").GetComponent<Question>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
