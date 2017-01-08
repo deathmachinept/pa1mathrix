bedc1309e5688ee1c050eb4287c674d53406b795
deathmachinept <deathmachine.travian@hotmail.com>
2017-01-08 17:26:33 +0000

    Merge RPG para poder trabalhar em desktop

---
+++ PA1 Mathrix/Assets/Scripts/RPG/Character/CharacterMovement.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CharacterMovement: MonoBehaviour
+{
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    private Direction currentDir;
+    private Vector2 input;
+    private bool isMoving = false;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float t;
+
+    public Sprite NorthSprite;
+    public Sprite EastSprite;
+    public Sprite SouthSprite;
+    public Sprite WestSprite;
+
+
+    public float walkSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (isMoving)
+	    {
+	        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	        if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+	        {
+	            input.y = 0;
+	        }
+	        else
+	        {
+	            input.x = 0;
+	        }
+
+	        if (input != Vector2.zero)
+	        {
+	            if (input.x < 0)
+	            {
+	                currentDir = Direction.West;
+	            }
+	            if (input.x > 0)
+	            {
+	               currentDir = Direction.East;
+	            }
+	            if (input.y < 0)
+	            {
+                    currentDir = Direction.South;
+
+	            }
+	            if (input.y > 0)
+	            {
+                    currentDir = Direction.North;
+
+	            }
+
+	            switch (currentDir)
+	            {
+	                case Direction.North:
+	                    gameObject.GetComponent<SpriteRenderer>().sprite = NorthSprite;
+                        break;
+                    case Direction.East:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = EastSprite;
+	                    break;
+                    case Direction.South:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = SouthSprite;
+                        break;
+                    case Direction.West:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = WestSprite;
+
+	                    break;
+
+
+	            }
+
+	            StartCoroutine(Move(transform));
+	        }
+	    }
+
+	}
+
+    public IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        t = 0;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x),startPos.y + System.Math.Sign(input.y),startPos.z);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime*walkSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
+
+
+
+}
